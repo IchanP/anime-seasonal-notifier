83cd1363603515ba78dd71aa6161169b57347e81
Pontus Grandin <pg222pb@student.lnu.se>
2023-10-02 18:35:52 +0200

    basic route

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/index.css'
 import App from './App'
+import ErrorPage from './pages/error-page'
+import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
